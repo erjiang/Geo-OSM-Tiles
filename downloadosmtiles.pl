d78c9a549279ddb04834c0615438a8d48ba0793d
Rolf Krahl <rotkraut@cpan.org>
2008-11-22 18:35:06 +0000

    Akzeptiere optional auch einen Permalink von OSM als Option.

--- downloadosmtiles.pl
+++ downloadosmtiles.pl
@@ -9,20 +9,32 @@ use File::Path;
 use File::Basename;
 use Getopt::Long;
 
-our $usage = qq{Usage: $0 --longitude=d --latitude=d --zoom=z};
+our $usage = qq{Usage: 
+   $0 --longitude=d --latitude=d --zoom=z
+   $0 --link=url [--longitude=d] [--latitude=d] [--zoom=z]
+};
 
 our %opt = (
     longitude => undef,
     latitude => undef,
     zoom => undef,
+    link => undef,
 );
 
 die "$usage\n"
     unless GetOptions(\%opt,
-                      "longitude=s", "latitude=s", "zoom=i") &&
+                      "longitude=s", "latitude=s", "zoom=i", "link=s") &&
            @ARGV == 0;
 
 sub downloadtile;
+
+if ($opt{link}) {
+    die "Invalid link: $opt{link}\n"
+	unless $opt{link} =~ /http:\/\/.*\/\?lat=([0-9]+\.[0-9]+)\&lon=([0-9]+\.[0-9]+)\&zoom=([0-9]+)/;
+    $opt{latitude} ||= $1;
+    $opt{longitude} ||= $2;
+    $opt{zoom} ||= $3;
+}
 
 # FIXME: Parsing of options and error check.
 
